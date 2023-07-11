4276f38275f42c9edca6980b5a4ac886a3454989
abdulmoiz-ahmer <abdulmoizahmer1996@gmail.com>
2023-07-11 23:58:18 +0500

    EDIT:

---
+++ src/App.vue
@@ -0,0 +1,126 @@
+<template>
+  <main class="l_container">
+    <section class="l_card">
+      <section class="l_about_us b_about_us">
+        <section class="b_about_us-left l_about_us-left">
+          <h1 class="b_about_us-left-title u_color-very-dark-magenta">
+            10,000+ of our users love our products.
+          </h1>
+          <p class="b_about_us-left-description u_color-grayish-magenta">
+            We only provide great products combined with excellent customer service. See what our
+            satisfied customers are saying about our services.
+          </p>
+        </section>
+        <section class="about_us-right">
+          <section
+            class="l_about_us-right-star_container b_about_us-right-star_container b_about_us-right-star_container_top u_p_25 u_bg-light-grayish-magenta"
+          >
+            <section class="l_about_us-right-star">
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+            </section>
+            <strong class="u_color-very-dark-magenta b_about_us-right-star_text"
+              >Rated 5 Stars in Reviews</strong
+            >
+          </section>
+          <section
+            class="l_about_us-right-star_container b_about_us-right-star_container b_about_us-right-star_container_middle u_p_25 u_bg-light-grayish-magenta"
+          >
+            <section class="l_about_us-right-star">
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+            </section>
+            <strong class="u_color-very-dark-magenta b_about_us-right-star_text"
+              >Rated 5 Stars in Report Guru</strong
+            >
+          </section>
+          <section
+            class="l_about_us-right-star_container b_about_us-right-star_container b_about_us-right-star_container_bottom u_p_25 u_bg-light-grayish-magenta"
+          >
+            <section class="l_about_us-right-star">
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+              <img src="./assets/icon-star.svg" alt="star-icon" class="u_mr-5" />
+            </section>
+            <strong class="u_color-very-dark-magenta b_about_us-right-star_text">
+              Rated 5 Stars in BestTech</strong
+            >
+          </section>
+        </section>
+      </section>
+      <section class="l_testimonials b_testimonials">
+        <section class="b_testimonials-left u_bg-very-dark-magenta">
+          <section class="b_testimonials-middle_header">
+            <figure>
+              <img src="./assets/image-colton.jpg" alt="colton-profile" class="avatar" />
+            </figure>
+            <span class="ml-25">
+              <h4 class="testimonials-left-testimony-name u_color-light-white">Colton Smith</h4>
+              <strong class="testimonials-left-testimony-title u_color-pink">Verified Buyer</strong>
+            </span>
+          </section>
+          <p
+            class="testimonials-left-testimony u_color-light-white font-size-16 mt-25 u_inline_block u_font_weight"
+          >
+            "We needed the same printed design as the one we had ordered a week prior. Not only did
+            they find the original order, but we also received it in time. Excellent!"
+          </p>
+        </section>
+        <section class="b_testimonials-middle u_bg-very-dark-magenta">
+          <section class="b_testimonials-middle_header">
+            <figure>
+              <img src="./assets/image-irene.jpg" alt="colton-profile" class="avatar" />
+            </figure>
+            <span class="b_testimonials-middle_header_info ml-25">
+              <h4 class="testimonials-left-testimony-name u_color-light-white font-size-16">
+                Irene Roberts
+              </h4>
+              <strong class="testimonials-left-testimony-title u_color-pink font-size-16"
+                >Verified Buyer</strong
+              >
+            </span>
+          </section>
+          <p
+            class="testimonials-left-testimony u_color-light-white font-size-16 mt-25 u_inline_block u_font_weight"
+          >
+            "Customer service is always excellent and very quick turn around. Completely delighted
+            with the simplicity of the purchase and the speed of delivery."
+          </p>
+        </section>
+        <section class="b_testimonials-right u_bg-very-dark-magenta">
+          <section class="b_testimonials-middle_header">
+            <figure>
+              <img src="./assets/image-anne.jpg" alt="colton-profile" class="avatar" />
+            </figure>
+            <span class="ml-25">
+              <h4 class="testimonials-left-testimony-name u_color-light-white font-size-16">
+                Anne Wallace
+              </h4>
+              <strong class="testimonials-left-testimony-title u_color-pink font-size-16"
+                >Verified Buyer</strong
+              >
+            </span>
+          </section>
+          <p
+            class="testimonials-left-testimony u_color-light-white font-size-16 mt-25 u_inline_block u_font_weight"
+          >
+            "Put an order with this company and can only praise them for the very high standard.
+            Will definitely use them again and recommend them to everyone!"
+          </p>
+        </section>
+      </section>
+    </section>
+  </main>
+</template>
+
+<style lang="scss">
+@import './assets/styles/index.scss';
+</style>
